df931a893861cff86a311d173fefe7e4d6463bd9
killerkewl <crticaldsa@gmail.com>
2024-12-17 09:56:09 -1000

    Update loader.js

--- static/assets/js/loader.js
+++ static/assets/js/loader.js
@@ -32,8 +32,7 @@ const themeStyles = {
   magma: { background: "rgb(31, 26, 26)" },
   sunset: { background: "rgb(29, 21, 27)" },
   midnight: { background: "rgb(27, 27, 27)" },
-  default: { background: "rgb(6, 22, 35)" },
-  linux: { backgroundImage: "url('https://i.etsystatic.com/49131988/r/il/111e0c/5800723226/il_fullxfull.5800723226_rvg7.jpg')" }
+  default: { background: "rgb(6, 22, 35)" }
 };
 
 const selectedStyle = themeStyles[selectedTheme] || themeStyles.default;
